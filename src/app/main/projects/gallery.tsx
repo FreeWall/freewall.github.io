f5b76214bfdf031540f42fe8b7a890271d6b21dd
FreeWall <info@michalvanek.net>
2022-09-28 12:17:56 +0200

    classes to functions

--- src/app/main/projects/gallery.tsx
+++ src/app/main/projects/gallery.tsx
@@ -1,7 +1,7 @@
 import styles from './gallery.less';
 import closeIcon from '../../../../assets/images/icons/close.inline.svg';
 import {ProjectProps} from './project';
-import {useEffect, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 
 interface GalleryProps {
     isVisible: boolean;
@@ -11,9 +11,47 @@ interface GalleryProps {
 
 export default function Gallery(props: GalleryProps) {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
-    const previewRef = React.createRef<HTMLDivElement>();
+    const currentImageIndexRef = useRef(currentImageIndex);
+
+    const previewRef = useRef<HTMLDivElement>();
 
     useEffect(() => {
+        currentImageIndexRef.current = currentImageIndex;
+    });
+
+    useEffect(() => {
+        const onWhell = (event: WheelEvent) => {
+            if (!props.isVisible) {
+                return;
+            }
+
+            for (let element of event.composedPath()) {
+                if (element == previewRef.current) {
+                    if (event.deltaY > 0) {
+                        if (currentImageIndexRef.current >= props.project.images.length - 1) {
+                            return;
+                        }
+
+                        setCurrentImageIndex(currentImageIndexRef.current + 1);
+                    } else {
+                        if (currentImageIndexRef.current < 1) {
+                            return;
+                        }
+
+                        setCurrentImageIndex(currentImageIndexRef.current - 1);
+                    }
+
+                    break;
+                }
+            }
+        };
+
+        const onKeyDown = (event: KeyboardEvent) => {
+            if (event.key == 'Escape') {
+                props.setVisible(false);
+            }
+        };
+
         document.addEventListener('keydown', onKeyDown);
         document.addEventListener('wheel', onWhell);
 
@@ -21,51 +59,7 @@ export default function Gallery(props: GalleryProps) {
             document.removeEventListener('keydown', onKeyDown);
             document.removeEventListener('wheel', onWhell);
         }
-    }, [props.isVisible, currentImageIndex]);
-
-    const onKeyDown = (event: KeyboardEvent) => {
-        if (event.key == 'Escape') {
-            props.setVisible(false);
-        }
-    };
-
-    const onWhell = (event: WheelEvent) => {
-        if (!props.isVisible) {
-            return;
-        }
-
-        for (let element of event.composedPath()) {
-            if (element == previewRef.current) {
-                if (event.deltaY > 0) {
-                    nextImage();
-                } else {
-                    previousImage();
-                }
-
-                break;
-            }
-        }
-    };
-
-    const nextImage = () => {
-        if (currentImageIndex >= props.project.images.length - 1) {
-            return;
-        }
-
-        changeImage(currentImageIndex + 1);
-    }
-
-    const previousImage = () => {
-        if (currentImageIndex < 1) {
-            return;
-        }
-
-        changeImage(currentImageIndex - 1);
-    }
-
-    const changeImage = (index) => {
-        setCurrentImageIndex(index);
-    }
+    }, [props.isVisible]);
 
     document.body.className = props.isVisible ? styles.disableScroll : '';
 
@@ -89,7 +83,7 @@ export default function Gallery(props: GalleryProps) {
                 </div>
                 <div className={styles.thumbnails}>
                     {props.project.images.map((thumbnail, index) => (
-                        <div className={styles.thumbnail + ' ' + (currentImageIndex == index ? styles.current : null)} onClick={() => changeImage(index)}>
+                        <div className={styles.thumbnail + ' ' + (currentImageIndex == index ? styles.current : null)} onClick={() => setCurrentImageIndex(index)}>
                             <img src={thumbnail.thumbnail} alt=""/>
                         </div>
                     ))}
