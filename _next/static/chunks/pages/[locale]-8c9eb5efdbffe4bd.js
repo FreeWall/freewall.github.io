f4769f3d6754fe4a6d29c0eb0c835f939dd2350f
FreeWall <FreeWall@users.noreply.github.com>
2023-05-19 13:21:41 +0000

    Deploying to gh-pages-newdark from @ FreeWall/freewall.github.io@55fcd60d4b4ba36f19dde2d39e2e0fbb9eb3e0ad 🚀

---
+++ _next/static/chunks/pages/[locale]-8c9eb5efdbffe4bd.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[513],{3124:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/[locale]",function(){return u(8818)}])}},function(n){n.O(0,[388,818,774,888,179],(function(){return _=3124,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
